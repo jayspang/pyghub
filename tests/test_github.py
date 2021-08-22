0082a00e8b301b7b09b3dfae6d05c5173ab1b4dd
Jay Spang <2514686+jayspang@users.noreply.github.com>
2021-08-21 19:20:13 -0700

    Rename main to cli, remove my API token

--- tests/test_github.py
+++ tests/test_github.py
@@ -2,7 +2,7 @@ import mock
 import pytest
 import unittest
 
-from pyghub.github import Github
+from pyghub.github import Github # pylint: disable=import-error
 
 class TestGithub(unittest.TestCase):
     def setup(self):
@@ -11,3 +11,7 @@ class TestGithub(unittest.TestCase):
     def test_fails_without_api_token(self):
         with pytest.raises(Exception):
             Github()
+
+    def test_does_not_fail_with_api_token(self):
+        gh = Github('fake_auth_token')
+        assert gh is not None
